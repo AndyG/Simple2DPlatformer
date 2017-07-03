2fe82dc499ca33709206b66da8aa9985f59c5cbd
andygarron <anderson.garron@gmail.com>
2017-07-03 10:36:21 -0700

    added rotation script

---
+++ Assets/Scripts/RotateSlow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSlow : MonoBehaviour {
+
+	public float degreesPerSec = 0f; 
+
+	void Start() { 
+	} 
+
+	void Update() { 
+		float rotAmount = degreesPerSec * Time.deltaTime; 
+		float curRot = transform.localRotation.eulerAngles.z; 
+		transform.rotation = Quaternion.Euler(new Vector3(0,0, curRot + rotAmount)); 
+	} 
+}
