bcf93ff9b647c9fc88dd7c0b8db840d943b935ba
andygarron <anderson.garron@gmail.com>
2017-07-03 19:27:25 -0700

    Added more mechanics

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour {
+
+	public GameObject target;
+	public float distance = 15;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerInfo = target.transform.transform.position;
+
+		transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - distance);
+	}
+}
